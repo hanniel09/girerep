3c8e638e1d91bf31bdf8e4d7634af23fc7e78098
hanniel09 <hannielvieira1227@gmail.com>
2025-05-30 20:54:53 -0300

    fix: correct cors problem in frontend

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- env: {
-  API_URL: process.env.API_URL || "http://localhost:8080",
-  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080",
- }
+//  env: {
+//   API_URL: process.env.API_URL || "http://localhost:8080",
+//   NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080",
+//  }
 };
 
 export default nextConfig;
